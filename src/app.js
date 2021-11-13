fabe3d38a393de8ebd4f3a2af89b673176a5676d
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 11:51:47 -0800

    Updated apiUrl link to include hyperlink protocol

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5678cd202ad8609baa4c102a770e20ac";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Reno&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
