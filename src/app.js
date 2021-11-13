9c90b0c93946554d46ee5f7e88c960e63f890963
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 12:31:12 -0800

    Added multiple weather elements, including humidity, windspeed, high and low temps, and main temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let mainTempElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#input-city-result");
+  let highLowElements = document.querySelector("#high-low-temps");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  mainTempElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  cityElement.innerHTML = response.data.name;
+  highLowElements.innerHTML = `${Math.round(
+    response.data.main.temp_max
+  )}° / ${Math.round(response.data.main.temp_min)}°`;
+  humidityElement.innerHTML = `${Math.round(response.data.main.humidity)}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
 }
 
 let apiKey = "5678cd202ad8609baa4c102a770e20ac";
