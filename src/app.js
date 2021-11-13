d74a6b6b88d173a2c40ac6467ae8751be311f8cc
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 11:44:55 -0800

    Added apiUrl and test function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "5678cd202ad8609baa4c102a770e20ac";
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Reno&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
