7d56d5ff7bfdb0ee79e947d36e1efda40ff528b6
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 17:59:11 -0800

    Created city element for future search engine function

--- src/app.js
+++ src/app.js
@@ -50,6 +50,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5678cd202ad8609baa4c102a770e20ac";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Reno";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
