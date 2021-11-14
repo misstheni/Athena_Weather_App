9d5c7168dbf91bb695e9a7049202a441a2aee277
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 17:51:45 -0800

    Added a date function, time function, and added more elements to displayTemperature function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let day = days[date.getDay()];
+  let month = months[date.getMonth()];
+  let dayInteger = date.getDate();
+  return `${day}, ${month} ${dayInteger}`;
+}
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let mainTempElement = document.querySelector("#current-temp");
@@ -6,6 +35,8 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#weather-description");
+  let dateElement = document.querySelector("#current-date");
+  let timeElement = document.querySelector("#current-time");
   mainTempElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
   cityElement.innerHTML = response.data.name;
   highLowElements.innerHTML = `${Math.round(
@@ -14,6 +45,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${Math.round(response.data.main.humidity)}%`;
   windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let apiKey = "5678cd202ad8609baa4c102a770e20ac";
