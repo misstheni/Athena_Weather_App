ad0b327d18ad16d257a36939a631fbcc97ee400e
misstheni <92562492+misstheni@users.noreply.github.com>
2021-11-13 22:09:13 -0800

    Updated time js to include proper digits and am or pm text

--- src/app.js
+++ src/app.js
@@ -24,6 +24,18 @@ function formatTime(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (hours >= 12) {
+    minutes = `${minutes} PM`;
+  } else {
+    minutes = `${minutes} AM`;
+  }
   return `${hours}:${minutes}`;
 }
 
